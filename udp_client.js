93287a71d76f867b3dfe1234154d7606d28f01e1
rinabekolli <94354965+rinabekolli@users.noreply.github.com>
2022-04-11 22:52:40 +0200

    Update udp_client.js

--- udp_client.js
+++ udp_client.js
@@ -13,3 +13,23 @@ client.on('message', function (msg, info) {
   console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
   promptForClientMessage();
 });
+
+
+promptForClientMessage();
+
+
+function promptForClientMessage() {
+  prompt.get('message', function (err, result) {
+    //sending msg
+    client.send(result.message, PORT, HOST, function(error){
+      
+      if(error){
+        console.log('An error has occurred!');
+        client.close();
+
+      } else {
+        console.log('Data sent !!!');
+      }
+    });
+  });
+}
