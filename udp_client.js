e42e204d1f9c11e1c2c2027cdca00e0a71baf2ed
rinabekolli <94354965+rinabekolli@users.noreply.github.com>
2022-04-11 22:51:51 +0200

    Update udp_client.js

--- udp_client.js
+++ udp_client.js
@@ -7,3 +7,9 @@ const HOST = '127.0.0.1';
 
 // creating a client socket
 var client = udp.createSocket('udp4');
+
+client.on('message', function (msg, info) {
+  console.log('Data received from server : ' + msg.toString());
+  console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port);
+  promptForClientMessage();
+});
