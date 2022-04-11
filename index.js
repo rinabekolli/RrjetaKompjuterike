07a236c1ec37e4b17e22fe4fcf4708158a412f8a
RinesaImeri <95044677+RinesaImeri@users.noreply.github.com>
2022-04-11 23:52:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ app.use(upload())
 app.get('/', (req, res) => {
       res.sendFile(__dirname + 'index.html')
 })
+
+app.post('/', (req,res) =>{
+if (req.files) {
+        console.log(req.files)
+        var file = req.files.file
+        var filename = file.name
+        console.log(filename)
+ 
+file.mv('./uploads/'+ filename, function (err) {
+if(err){
+ res.send(err)
+} else {
+res.send("File Uploaded")
+      }
+   })
+}
+})
+ app.listen(2222)
