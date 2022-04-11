4e076062487137eaf01aed16598abf50b40d63d2
RinesaImeri <95044677+RinesaImeri@users.noreply.github.com>
2022-04-11 22:37:28 +0200

    Update udp_client.js

--- udp_client.js
+++ udp_client.js
@@ -1,1 +1,9 @@
+
 const udp = require('dgram');
+const prompt = require("prompt");
+
+const PORT = 2222;
+const HOST = '127.0.0.1';
+
+// creating a client socket
+var client = udp.createSocket('udp4');
