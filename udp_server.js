2441ca66e815330528215aa3e8680bed9b7028ef
Qendresa-Potoku <94839915+Qendresa-Potoku@users.noreply.github.com>
2022-04-11 22:52:53 +0200

    Update udp_server.js

--- udp_server.js
+++ udp_server.js
@@ -1,1 +1,21 @@
 const udp = require('dgram');
+var fs = require('fs');
+
+//creating a udp server
+var server=udp.createSocket('udp4');
+
+const PORT=2222;
+const HOST='localhost';
+
+server.bind(PORT,HOST);
+
+//emits when any error occurs
+server.on('error',function(error){
+  console.log('Error: ' + error);
+  server.close();
+});
+
+server.on('connect', () => {
+  console.log('A client just connected');
+})
+
